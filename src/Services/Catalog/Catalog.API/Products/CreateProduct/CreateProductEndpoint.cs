02b870d9781ed98e578e8e7a3dd455c19f067452
Marm485 <miguelribeiro485@gmail.com>
2025-01-07 00:42:51 +0000

    add CreateProduct feature setup with MediaTR

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public static class CreateProductEndpoint
+{
+
+    public static void MapCreateProductEndpoint(this IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("/", () =>
+        {
+            return Results.NoContent();
+        });
+    }
+
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : IRequest<CreateProductCommandResult>;
+
+public record CreateProductCommandResult(Guid Id);
+
+internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductCommandResult>
+{
+    public Task<CreateProductCommandResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
